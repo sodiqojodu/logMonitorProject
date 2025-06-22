b1ef0158de9b5f09048a5bc3285497e17bcd5560
Olawale Ojodu <ojso1@outlook.com>
2025-06-22 23:13:57 +0100

    Added an entry point

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import logging
+from logParser import parseLogFile
+from reportCreator import createReport
+
+
+def main():
+    """
+    using sys.argv to determine the log file path.
+    uses the argument provided, otherwise defaults to logs.log
+    """
+    if len(sys.argv) > 1:
+        logFile = sys.argv[1]
+    else:
+        logFile =  "logs[14].log"
+
+    try:
+        jobs = parseLogFile(logFile)
+    except FileNotFoundError:
+        logging.error(f"{logFile} file not found")
+        sys.exit(1)
+
+    report = createReport(jobs)
+    print(report)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    main()
